0e63cb5435b649b144f0eafa60953b54666c9d54
ardianhilmip35 <ardianhil86@gmail.com>
2022-03-23 10:45:39 +0700

    hilmi commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,9 +13,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home', [
-            'title' => 'Home',
-        ]);
+        return view('home');
     }
 
     /**
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('/layout/main')
+
+@section('title', 'Home')
+@section('content')
+    
+@endsection
