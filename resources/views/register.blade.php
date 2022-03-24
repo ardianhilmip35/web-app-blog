dd178d27602bf8ae549b609e5a44b0da6591ebb9
ardianhilmip35 <ardianhil86@gmail.com>
2022-03-24 13:11:44 +0700

    hilmi commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,32 +3,34 @@
 @section('title', 'Register')
 @section('content')
 
-<div class="container ">
-    <form class="row justify-content-center fmreg shadow" novalidate>
-        <div class="col-md-4 mb-3">
-          <input type="text" class="form-control" id="validationCustom01" value="" placeholder="FIRST NAME" required>
+    <form class="row justify-content-center fmreg shadow" novalidate> 
+        <div class="col-md-8 text-center mb-1"> 
+            <img class="mb-3" src="/img/LOGO TOK.png" alt="" id="logoreglog">
+            <p class="txtreg">Please Register Your Account</p>  
         </div>
-        <div class="col-md-4 mb-3">
-          <input type="text" class="form-control" id="validationCustom02" value="" placeholder="LAST NAME" required>
+        <div class="col-md-5 mb-3">
+            <input type="text" class="form-control" id="validationCustom01" value="" placeholder="Nama Depan" required>
         </div>
-        <div class="col-md-8 mb-3">
-          <input type="text" class="form-control mb-3" id="validationCustom03" value="" placeholder="Email" required>
-          <input type="text" class="form-control" id="validationCustom03" value="" placeholder="Password" required>
+        <div class="col-md-5 mb-3">
+            <input type="text" class="form-control" id="validationCustom02" value="" placeholder="Nama Belakang" required>
         </div>
-        <div class="col-md-8 mb-4">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-            <label class="form-check-label" for="invalidCheck">
-              Agree to terms and conditions
-            </label>
-            <div class="invalid-feedback">
-              You must agree before submitting.
+        <div class="col-md-10 mb-3">
+            <input type="text" class="form-control mb-3" id="validationCustom03" value="" placeholder="Email" required>
+            <input type="text" class="form-control" id="validationCustom03" value="" placeholder="Password" required>
+        </div>
+        <div class="col-md-10 mb-4">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                <label class="form-check-label" for="invalidCheck">
+                    Agree to terms and conditions
+                </label>
+                <div class="invalid-feedback">
+                    You must agree before submitting.
+                </div>
             </div>
-          </div>
         </div>
         <div class="col-md-8 text-center">
-          <button class="btn btn-primary" type="submit">Register</button>
+            <button class="btn btn-primary" type="submit">Register</button>
         </div>
-      </form>
-</div>
+    </form>
 @endsection
